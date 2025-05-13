a2edf48afbb55af6b542aad0edc1844eb2b91972
mariapretelt <m.pretelt@udea.edu.co>
2025-05-13 00:46:33 -0500

    Matriz 3d copy

--- taller.py
+++ taller.py
@@ -1,4 +1,8 @@
 #Crear una matriz de Numpy aleatoria de 4 dimensiones y un size de 1200000
 import numpy as np
 matriz=np.random.rand(10,15,20,40)
-print(matriz)
+# print(matriz)
+
+#Crea una copia de la matriz creada en el ítem anterior (usar método copy) de solo 3 dimensiones (“Cortando una de las dimensiones”)
+matriz_3d= matriz[:, 0, :, :].copy() #15
+print(matriz_3d)
